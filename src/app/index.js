8e14b665f0cbb1913f73721979211ae3a92a3d87
komondor <komondor.mtl@gmail.com>
2016-11-03 23:20:24 -0400

    removing semi-colon

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,13 @@
-'use strict';
 
-var Q = require('q');
-var fs = require('fs');
-var _ = require('underscore');
-var inquirer = require("inquirer");
+var Q = require('q')
+var fs = require('fs')
+var _ = require('underscore')
+var inquirer = require("inquirer")
 
 
 
 //export
-module.exports 	= SW;
+module.exports 	= SW
 
 function SW(){this.possiblematch = []}
 
@@ -16,67 +15,67 @@ function SW(){this.possiblematch = []}
 // resolve: array of matches
 // reject: no match
 SW.prototype.readfolder =  function (data) {
-	var self = this;
-	var urlnow = data[0];
-	var word = data[1];
-  var thatDeffered = Q.defer();
+	var self = this
+	var urlnow = data[0]
+	var word = data[1]
+	var thatDeffered = Q.defer()
 
 	// intercept ..
 	if(word === ".."){
-		self.possiblematch = [];
-		let resultUrl = urlnow+word+'/';
-		thatDeffered.resolve([resultUrl]);
+		self.possiblematch = []
+		let resultUrl = urlnow+word+'/'
+		thatDeffered.resolve([resultUrl])
 	}
 
 	fs.readdir(urlnow, function(err,success){
 		_.each(success, function(argu){
-			var stats = fs.statSync(urlnow+argu);
+			var stats = fs.statSync(urlnow+argu)
 			if(argu.search(new RegExp(word, "i"))>-1 && stats.isDirectory())
-				self.possiblematch.push(urlnow+argu+'/');
-		});
+				self.possiblematch.push(urlnow+argu+'/')
+		})
 		if(self.possiblematch.length ===0){
-			self.possiblematch = [];
-			thatDeffered.resolve(null);
+			self.possiblematch = []
+			thatDeffered.resolve(null)
 		}
 		else {
-			self.result = self.possiblematch;
-			self.possiblematch = [];
-			thatDeffered.resolve(self.result);
+			self.result = self.possiblematch
+			self.possiblematch = []
+			thatDeffered.resolve(self.result)
 		}
-	});
-	return thatDeffered.promise;
+	})
+	return thatDeffered.promise
 }
 
 // once the iteration process is completed
 // and matches.length > 1
 // ask user to choose folder
 SW.prototype.questionuser = function (choices) {
-	var thatDeffered = Q.defer();
+	var thatDeffered = Q.defer()
 	var questionfolder = [{
 		type: "list",
 		name: "folderChoice",
 		message: "Which folder would like to work with?",
 		choices: choices
-	}];
+	}]
 
 	inquirer.prompt( questionfolder, function( answers ) {
-		thatDeffered.resolve(answers.folderChoice);
-	});
-	return thatDeffered.promise;
+		thatDeffered.resolve(answers.folderChoice)
+	})
+	return thatDeffered.promise
 }
 
 // iteration
 SW.prototype.iteration =  function (list,url)  {
-	var self = this;
-	var thatDeffered = Q.defer();
-	var starting_path = ['./'];
+	var self = this
+	var thatDeffered = Q.defer()
+	var starting_path = ['./']
 	let home_directory = getUserHome()
-	console.log(home_directory);
+	console.log(home_directory)
 
 	if(list[0]=== ':') {
 
-		list = list.slice(1);
-		starting_path = [home_directory + '/'];
+		list = list.slice(1)
+		starting_path = [home_directory + '/']
 
 		if(list.length === 0) {
 			thatDeffered.resolve([starting_path])
@@ -85,57 +84,57 @@ SW.prototype.iteration =  function (list,url)  {
 	}
 
 	var sequenceKeyword = Q().then(()=>{
-			return [starting_path,list]
-	});
+		return [starting_path,list]
+	})
 
 	for(let i=0;i<list.length;i++){
-		let resultat = [];
+		let resultat = []
 		sequenceKeyword=sequenceKeyword.then(function(data){
-			var keywordDeffered = Q.defer();
+			var keywordDeffered = Q.defer()
 			var sequenceFolder = Q().then(function(){
-				return [data[0][0],list[i]];
-			});
+				return [data[0][0],list[i]]
+			})
 
 			for(let j=0;j<data[0].length;j++){
 				sequenceFolder = sequenceFolder
 				.then(self.readfolder.bind(self))
 				.then(function(datafrom){
-					var folderDiffered = Q.defer();
+					var folderDiffered = Q.defer()
 					if(j !== data[0].length-1){
 						if(datafrom)
-							datafrom.forEach((fromiter)=>{resultat.push(fromiter)});
-						folderDiffered.resolve([data[0][j+1],list[i]]);
+							datafrom.forEach((fromiter)=>{resultat.push(fromiter)})
+						folderDiffered.resolve([data[0][j+1],list[i]])
 					}
 					if(j === data[0].length-1 && i !== list.length -1){
 						if(!datafrom && resultat.length ===0)
-							thatDeffered.reject('no match');
+							thatDeffered.reject('no match')
 						if(datafrom)
-							datafrom.forEach((fromiter)=>{resultat.push(fromiter)});
-						keywordDeffered.resolve([resultat]);
+							datafrom.forEach((fromiter)=>{resultat.push(fromiter)})
+						keywordDeffered.resolve([resultat])
 					}
- 					if(j === data[0].length-1 && i === list.length -1){
+					if(j === data[0].length-1 && i === list.length -1){
 						if(!datafrom && resultat.length ===0)
-							thatDeffered.reject('no match');
+							thatDeffered.reject('no match')
 						if(datafrom)
-							datafrom.forEach((fromiter)=>{resultat.push(fromiter)});
-						thatDeffered.resolve(resultat);
+							datafrom.forEach((fromiter)=>{resultat.push(fromiter)})
+						thatDeffered.resolve(resultat)
 					}
-					return folderDiffered.promise;
-				});
+					return folderDiffered.promise
+				})
 			}
 			sequenceFolder
 				.catch((err)=>{thatDeffered.reject(err)})
-				.done();
-			return keywordDeffered.promise;
-		});
+				.done()
+			return keywordDeffered.promise
+		})
 
 	}
 	sequenceKeyword
 		.catch((err)=>{thatDeffered.reject(err)})
-		.done();
-	return thatDeffered.promise;
+		.done()
+	return thatDeffered.promise
 }
 
 function getUserHome() {
-  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+	return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']
 }
